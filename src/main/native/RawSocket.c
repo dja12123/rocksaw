cb4553b50b4cb53faab65565f75d10cab5ae018e
dja12123 <dja12123@dja-univ-main>
2018-11-22 00:43:42 +0900

    test

--- src/main/native/RawSocket.c
+++ src/main/native/RawSocket.c
@@ -36,7 +36,7 @@
 #  include <net/ethernet.h>
 #  include <sys/ioctl.h>
 #  include <linux/if_packet.h>
-#  include <net/if.h>
+#  include <linux/if.h>
 #  include <arpa/inet.h>
 
 #  ifdef ROCKSAW_USE_POLL
