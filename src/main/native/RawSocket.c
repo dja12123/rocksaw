c1339ce3dde190c9a4c546937c5dc48a4fe2a2f1
dja12123 <dja12123@dja-univ-main>
2018-11-22 00:45:48 +0900

    test

--- src/main/native/RawSocket.c
+++ src/main/native/RawSocket.c
@@ -37,8 +37,19 @@
 #  include <sys/ioctl.h>
 #  include <linux/if_packet.h>
 #  include <linux/if.h>
-#  include <netinet/ether.h>
 
+
+#include <arpa/inet.h>
+#include <linux/if_packet.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <netinet/ether.h>
 #  ifdef ROCKSAW_USE_POLL
 
 #    include <poll.h>
